b7efc41db1e4d6faf8df5be0a7667dd42e6d4c7d
Padel <maxsportevents@gmail.com>
2023-09-15 21:07:13 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,68 +1,62 @@
+/* Redireccionar imagen */
+const imagen = document.getElementById('logo');
 
-// open menu desplegable
+imagen.addEventListener('click', function() {
+    window.location.href = '..';
+});
+
+/* menu desplegable */
 function openMenu() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('show');
 }
-
-// close menu desplegable
 function closeMenu() {
     var menu = document.getElementById('menu');
     menu.classList.remove('show');
   }
 
-  /* unlock row */
-  window.onscroll = function() {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("scroll-to-top").style.display = "block";
-    } else {
-      document.getElementById("scroll-to-top").style.display = "none";
-    }
-  }
 
 
-  function abrirImagen(src) {
-    // Obtener el elemento modal y la imagen
-    const modal = document.getElementById("imagen-modal");
-    const imagenAmpliada = document.getElementById("imagen-ampliada");
+/* unlock scroll row*/
+window.onscroll = function() {
+  scrollFunction();
+};
   
-    // Asignar la imagen seleccionada al elemento modal
-    imagenAmpliada.src = src;
-  
-    // Mostrar el modal
-    modal.style.display = "block";
-    ocultarEncabezado();
+function scrollFunction() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scroll-to-top").style.display = "block";
+  } else {
+    document.getElementById("scroll-to-top").style.display = "none";
   }
-  
-  function cerrarImagen() {
-    // Obtener el elemento modal
-    const modal = document.getElementById("imagen-modal");
-  
-    // Ocultar el modal
-    modal.style.display = "none";
-    mostrarEncabezado();
-  }
+}
 
-  function ocultarEncabezado() {
+
+/* Imagen ampliada */
+function abrirImagen(src) {
+  const modal = document.getElementById("imagen-modal");
+  const imagenAmpliada = document.getElementById("imagen-ampliada");
+  // Asignar la imagen seleccionada al elemento modal
+  imagenAmpliada.src = src;
+  // Mostrar el modal
+  modal.style.display = "block";
+  ocultarEncabezado();
+}
+  
+function cerrarImagen() {
+  // Obtener el elemento modal
+  const modal = document.getElementById("imagen-modal");
+  // Ocultar el modal
+  modal.style.display = "none";
+   mostrarEncabezado();
+}
+
+
+/* Ocultar y mostrar el encabezado*/
+function ocultarEncabezado() {
     const encabezado = document.getElementById("header");
-    // Cambiar el estilo para ocultar el encabezado
     encabezado.style.display = "none";
 }
-
 function mostrarEncabezado() {
   const encabezado = document.getElementById("header");
-  // Cambia el estilo para mostrar el encabezado
   encabezado.style.display = "block";
 }
-
-
- // Redireccionar imagen
- const imagen = document.getElementById('milogo');
-
- imagen.addEventListener('click', function() {
-     window.location.href = '..';
- });
